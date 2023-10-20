94c542a81d5af48e0574a8470e37fe5d17dfcff4
Atte Kivisto <atte.t.kivisto@gmail.com>
2023-10-20 17:42:01 +0300

    front-end started

---
+++ frontend-lk/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import PrimarySearchAppBar from './Appbar'
+
+function App() {
+  return (
+    <>
+      <PrimarySearchAppBar />
+    </>
+  )
+}
+
+export default App
+
