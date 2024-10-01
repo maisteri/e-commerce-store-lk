0d8b6a9affbce06ba8c46f64dc7339f4c3e518c9
Atte Kivistö <atte.t.kivisto@gmail.com>
2024-10-01 16:10:08 +0300

    env config changed

--- frontend-lk/src/config.ts
+++ frontend-lk/src/config.ts
@@ -1,4 +1,6 @@
+const env = import.meta.env.VITE_ENV || 'production'
+
 export const apiBaseUrl =
-  import.meta.env.VITE_ENV === 'development'
+  env === 'development'
     ? 'http://localhost:3000/v1/api'
     : 'https://e-commerce-store-lk.onrender.com/v1/api'
